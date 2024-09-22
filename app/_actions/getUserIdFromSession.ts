552db56d1a6ac848bccddd781d1adb9fe8d61cbd
Hudson256 <hudsono256@gmail.com>
2024-09-22 20:35:19 -0300

    feat: retrieve user ID from database using email from session

--- app/_actions/getUserIdFromSession.ts
+++ app/_actions/getUserIdFromSession.ts
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/app/_lib/auth"
 import { logger } from "@/app/_lib/logger"
+import { db } from "@/app/_lib/prisma"
 
 export async function getUserIdFromSession(
   request: Request,
@@ -8,14 +9,20 @@ export async function getUserIdFromSession(
   try {
     const session = await getServerSession({ req: request, ...authOptions })
     logger.log("User session:", session)
-    const userId = session?.user?.id
-
-    if (!userId) {
-      logger.error("User ID is missing")
+    const email = session?.user?.email
+    if (!email) {
+      logger.error("User email is missing")
       return null
     }
-
-    return userId
+    const user = await db.user.findUnique({
+      where: { email: email },
+      select: { id: true },
+    })
+    if (!user) {
+      logger.error("User not found in the database")
+      return null
+    }
+    return user.id
   } catch (error) {
     logger.error("Error retrieving user ID:", error)
     return null
